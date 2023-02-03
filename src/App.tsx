a901d3c00dc70cf9fcb5d5e993664950128a427f
MichaelB <mjbradvica@protonmail.com>
2023-02-02 20:59:03 -0500

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-const App:React.FC = () => {
-  return (
-    <div>App</div>
-  );
-}
+const App: React.FC = () => {
+  return <div>App</div>;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') ?? new Element()
+  document.getElementById("root") ?? new Element()
 );
 root.render(
   <React.StrictMode>
