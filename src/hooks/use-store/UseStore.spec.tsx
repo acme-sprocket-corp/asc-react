cc88616cdf4554b8b8d80c3e960140bcc2dd9653
MichaelB <mjbradvica@protonmail.com>
2023-02-10 13:06:10 -0500

    updated store tests

--- src/hooks/use-store/UseStore.spec.tsx
+++ src/hooks/use-store/UseStore.spec.tsx
@@ -1,40 +1,47 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
+import { act } from "@testing-library/react";
 import { ApplicationContext } from "../../stores/ApplicationStore";
-import applicationReducer from "../../stores/reducer/ApplicationReducer";
-import { defaultApplicationState } from "../../stores/state/ApplicationState";
-import useCreateStore from "../use-create-store/UseCreateStore";
 import useStore from "./UseStore";
+import { renderHook } from "@testing-library/react";
+import {
+  ApplicationState,
+  defaultApplicationState,
+} from "../../stores/state/ApplicationState";
+import { ReactNode, useReducer } from "react";
+import applicationReducer from "../../stores/reducer/ApplicationReducer";
 
-const TestComponent: React.FC = () => {
-  const [store] = useStore();
-
-  return (
-    <>
-      <p>LoggedIn: {store.isLoggedIn ? "in" : "out"}</p>
-      <p>Email: {store.customerEmail === "" ? "no email" : "email"}</p>
-    </>
-  );
-};
-
-const Wrapper: React.FC = () => {
-  const useStore = useCreateStore(
-    defaultApplicationState(),
-    applicationReducer
+const StoreWrapper: React.FC<StoreWrapperProps> = ({ children }) => {
+  const [state, dispatcher] = useReducer(
+    applicationReducer,
+    defaultApplicationState()
   );
 
   return (
-    <ApplicationContext.Provider value={useStore}>
-      <TestComponent />
+    <ApplicationContext.Provider
+      value={{
+        state,
+        dispatcher,
+      }}
+    >
+      {children}
     </ApplicationContext.Provider>
   );
 };
 
+interface StoreWrapperProps {
+  children: ReactNode;
+}
+
 describe("UseStore", () => {
   it("has correct state", () => {
-    render(<Wrapper />);
+    const { result } = renderHook(() => useStore(), { wrapper: StoreWrapper });
 
-    expect(screen.getByText(/out/i)).toBeInTheDocument();
-    expect(screen.getByText(/no email/i)).toBeInTheDocument();
+    let state: ApplicationState;
+
+    act(() => {
+      state = result.current[0];
+
+      expect(state.isLoggedIn).toStrictEqual(false);
+      expect(state.customerEmail).toStrictEqual("");
+    });
   });
 });
